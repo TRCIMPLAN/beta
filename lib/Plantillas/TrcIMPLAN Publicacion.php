57a055fa2b768b56afcd65a2cf22a5bd4a90cd82
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2016-03-08 16:14:37 -0600

    Nuevas Plantillas.

---
+++ lib/Plantillas/TrcIMPLAN Publicacion.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - CLASE
+ *
+ * Copyright (C) 2016 IMPLAN Torreón
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace DIRECTORIO;
+
+/**
+ * Clase CLASE
+ */
+class CLASE extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre      = 'Título';
+     // $this->autor       = '';
+        $this->fecha       = '2016-00-00T00:00';
+        // El nombre del archivo a crear
+        $this->archivo     = 'titulo';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion = 'Descripción.';
+        $this->claves      = 'Clave1, Clave2, Clave3';
+        // Para el Organizador
+        $this->categorias  = array('Categoria1', 'Categoria2', 'Categoria3');
+    } // constructor
+
+    /**
+     * HTML
+     *
+     * @return string Código HTML
+     */
+    public function html() {
+        // Contenido
+        $this->contenido = <<<FINAL
+FINAL;
+        // Ejecutar este método en el padre
+        return parent::html();
+    } // html
+
+    /**
+     * Javascript
+     *
+     * @return string Código Javascript
+     */
+    public function javascript() {
+        // JavaScript
+        $this->javascript = <<<FINAL
+FINAL;
+        // Ejecutar este método en el padre
+        return parent::html();
+    } // javascript
+
+    /**
+     * Redifusion HTML
+     *
+     * @return string Código HTML
+     */
+    public function redifusion_html() {
+        // Código HTML para redifusión
+        $this->redifusion = <<<FINAL
+FINAL;
+        // Ejecutar este método en el padre
+        return parent::redifusion_html();
+    } // redifusion_html
+
+} // Clase CLASE
+
+?>
